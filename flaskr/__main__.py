a60fd0398870c972e88844a23bb92e0ed91d8e08
Caleb Carlson <ccarlson355@gmail.com>
2022-01-12 11:25:41 -0700

    Load GISJOINs at startup

--- flaskr/__main__.py
+++ flaskr/__main__.py
@@ -1,13 +1,40 @@
 import os
+import json
 from flask import Flask, request
 from werkzeug.utils import secure_filename
 from http import HTTPStatus
+from pprint import pprint
 
 UPLOAD_DIR = './uploads'
+RESOURCES_DIR = './flaskr/resources'
 ALLOWED_EXTENSIONS = {'zip'}
+COUNTY_GISJOINS = []
 
 app = Flask(__name__)
 app.config['UPLOAD_DIR'] = UPLOAD_DIR
+
+
+# Main entrypoint
+def main():
+    print("Running main() function...")
+    global COUNTY_GISJOINS
+    COUNTY_GISJOINS = load_gis_joins()
+    print(f"Loaded {len(COUNTY_GISJOINS)} county GISJOINs")
+    app.run()  # Entrypoint
+
+
+def load_gis_joins():
+    county_gis_joins = []
+    gis_join_filename = f"{RESOURCES_DIR}/gis_joins.json"
+    with open(gis_join_filename, "r") as read_file:
+        print("Loading in list of county GISJOINs...")
+        gis_joins = json.load(read_file)
+        states = gis_joins["states"]
+        for state_key, state_value in states.items():
+            for county_key, county_value in state_value["counties"].items():
+                county_gis_joins.append(county_value["GISJOIN"])
+
+    return county_gis_joins
 
 
 def allowed_file(filename):
@@ -22,6 +49,9 @@ def hello():
 
 @app.route('/validation', methods=['POST'])
 def validation():
+
+    pprint(request.form)
+    print(request.form["test_key"])
 
     # Check if the POST request has the file part
     if 'file' not in request.files:
@@ -39,3 +69,7 @@ def validation():
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_DIR'], filename))
         return f'File {filename} successfully saved', HTTPStatus.OK
+
+
+if __name__ == "__main__":
+    main()
--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@
 echo -e "Checking dependencies..."
 python3 -m pip list | grep "Flask" || python3 -m pip install --user -r requirements.txt
 
-export FLASK_APP=flaskr
-flask run
+#export FLASK_APP=flaskr
+#flask run
+python3 flaskr
