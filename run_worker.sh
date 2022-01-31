56ea564703e10116be3f52f8c41c3a5bf8aefc3e
Caleb Carlson <ccarlson355@gmail.com>
2022-01-31 10:26:01 -0700

    Update run worker script

--- run_worker.sh
+++ run_worker.sh
@@ -10,4 +10,4 @@ function print_usage {
 MASTER_URI="$1"
 WORKER_PORT="$2"
 
-python3 overlay --worker --master_uri="$MASTER_HOST" --port="$WORKER_PORT"
+python3 overlay --worker --master_uri="$MASTER_URI" --port="$WORKER_PORT"
