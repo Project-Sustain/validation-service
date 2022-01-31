1e52c5b1038704ee583fc6c9eee0a2c3ee884a0e
Menuka Warushavithana <menuka.warushavithana@gmail.com>
2022-01-31 02:16:08 -0700

    build: add env.sh.example

--- overlay/scikitlearn/wireformats/validation_request.py
+++
@@ -1,7 +0,0 @@
-class ValidationRequest:
-    def __init__(self, collection: str, gis_join: str, dependent_vars: list, independent_vars, model):
-        self.collection = collection
-        self.gis_join = gis_join
-        self.dependent_vars = dependent_vars
-        self.independent_vars = independent_vars
-        self.model = model
