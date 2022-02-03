fd001d999da3d37236dde4240713aaf91a35601f
Caleb Carlson <ccarlson355@gmail.com>
2022-02-03 15:55:49 -0700

    Relative imports 3

--- overlay/db/shards.py
+++ overlay/db/shards.py
@@ -2,8 +2,8 @@ import sys
 from pymongo import MongoClient
 from logging import info, error
 
-sys.path.append('../../overlay')
-from overlay.constants import DB_HOST, DB_PORT
+sys.path.append('../..')
+from overlay.constants import DB_HOST, DB_PORT, DB_NAME
 
 
 class ShardMetadata:
