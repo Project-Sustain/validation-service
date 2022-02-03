8fc476a71b924976b7b202d1a893a3ff71b5166c
Caleb Carlson <ccarlson355@gmail.com>
2022-02-03 09:51:04 -0700

    Add kill_worker script

--- kill_worker.sh
+++ kill_worker.sh
@@ -5,9 +5,9 @@ WORKER_PROCESS=$(ps -aux | grep "[r]un_worker.sh")
 if [[ $WORKER_PROCESS != "" ]]; then
   echo "$WORKER_PROCESS"
   WORKER_PID=$(echo "$WORKER_PROCESS" | awk '{ print $2 }')
-  echo "Found worker process with pid $WORKER_PID"
 
   kill "$WORKER_PID"
+  echo "Found and killed worker process with pid $WORKER_PID"
 else
   echo "Did not find any worker processes to kill"
 fi
