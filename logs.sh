c0911656203d0b8e23f22ab21503cda6d18093c8
Caleb Carlson <ccarlson355@gmail.com>
2022-03-16 13:14:08 -0600

    Fix log script

--- logs.sh
+++ logs.sh
@@ -5,8 +5,6 @@ function print_usage {
   echo -e "Follows the logs of the current validation service process"
 }
 
-if [[ $# -eq 0 ]]; then
-  tail -n 200 -f log.txt
-else
-  print_usage
-fi
+[[ $1 == "--help" ]] && print_usage && exit 0
+
+tail -n 200 -f log.txt
