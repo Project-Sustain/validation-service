31cc5bb9df444f7d3b7a51588c74f44f4551e9b0
Caleb Carlson <ccarlson355@gmail.com>
2022-03-23 11:43:28 -0600

    Test TF loss func

--- overlay/tensorflow_validation/main.py
+++ overlay/tensorflow_validation/main.py
@@ -5,7 +5,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
-import pandas
 import time
 import h5py
 from pprint import pprint
@@ -19,12 +18,15 @@ URI = "mongodb://lattice-100:27018/"
 DATABASE = "sustaindb"
 COLLECTION = "noaa_nam"
 GIS_JOIN = "G3500170"
-FEATURE_FIELDS = [
+REGRESSION_FEATURE_FIELDS = [
         "PRESSURE_AT_SURFACE_PASCAL",
         "RELATIVE_HUMIDITY_2_METERS_ABOVE_SURFACE_PERCENT"
 ]
-LABEL_FIELD = "TEMPERATURE_AT_SURFACE_KELVIN"
-CLASSIFICATION_FIELD = "CATEGORICAL_RAIN_SURFACE_BINARY"
+CLASSIFICATION_FEATURE_FIELDS = [
+
+]
+REGRESSION_LABEL_FIELD = "TEMPERATURE_AT_SURFACE_KELVIN"
+CLASSIFICATION_LABEL_FIELD = "CATEGORICAL_RAIN_SURFACE_BINARY"
 
 # Modeling Stuff
 LEARNING_RATE = 0.001
@@ -32,7 +34,7 @@ EPOCHS = 3
 BATCH_SIZE = 32
 
 
-def create_and_train_model(features_df, label_df) -> tf.keras.Model:
+def create_and_train_regression_model(features_df, label_df) -> tf.keras.Model:
     model = tf.keras.Sequential()
     model.add(tf.keras.Input(shape=(2,)))
     model.add(tf.keras.layers.Dense(units=16, activation="relu", name="first_layer"))
@@ -65,34 +67,69 @@ def create_and_train_classification_model(features_df, label_df) -> tf.keras.Mod
     return model
 
 
-def main():
-    logging.basicConfig(level=logging.INFO)
-
-    info("tensorflow version: {}".format(tf.__version__))
-
+def get_data_for_regression_model() -> (pd.DataFrame, pd.DataFrame):
     # Load in data
     client = MongoClient(URI)
     database = client["sustaindb"]
     collection = database["noaa_nam"]
     match = {"GISJOIN": GIS_JOIN}
-    projection = {"_id": 0, CLASSIFICATION_FIELD: 1}
-    for feature_field in FEATURE_FIELDS:
+    projection = {"_id": 0, REGRESSION_LABEL_FIELD: 1}
+    for feature_field in REGRESSION_FEATURE_FIELDS:
         projection[feature_field] = 1
     documents = collection.find(match, projection)
     features_df = pd.DataFrame(list(documents))
     scaled = MinMaxScaler(feature_range=(0, 1)).fit_transform(features_df)
     features_df = pd.DataFrame(scaled, columns=features_df.columns)
-    label_df = features_df.pop(CLASSIFICATION_FIELD)
+    label_df = features_df.pop(REGRESSION_LABEL_FIELD)
 
-    # model: tf.keras.Model = create_and_train_model(features_df, label_df)
-    # model.save("my_model.h5")
-    classification_model: tf.keras.Model = create_and_train_classification_model(features_df, label_df)
-    classification_model.save("my_classification_model.h5")
+    return features_df, label_df
 
-    loaded_model: tf.keras.Model = tf.keras.models.load_model("my_classification_model.h5")
-    loaded_model.summary()
 
-    #validation_results = loaded_model.evaluate(features_df, label_df, batch_size=128, return_dict=True, verbose=1)
+def get_data_for_classification_model() -> (pd.DataFrame, pd.DataFrame):
+    # Load in data
+    client = MongoClient(URI)
+    database = client["sustaindb"]
+    collection = database["noaa_nam"]
+    match_with = {"GISJOIN": GIS_JOIN, CLASSIFICATION_LABEL_FIELD: 1}
+    projection = {"_id": 0, CLASSIFICATION_LABEL_FIELD: 1}
+    for feature_field in CLASSIFICATION_FEATURE_FIELDS:
+        projection[feature_field] = 1
+    documents_with = collection.find(match_with, projection)
+    features_df_with = pd.DataFrame(list(documents_with))
+
+    match_without = {"GISJOIN": GIS_JOIN, CLASSIFICATION_LABEL_FIELD: 0}
+    documents_without = collection.find(match_without, projection)
+    features_df_without = pd.DataFrame(list(documents_without)).limit(features_df_with.shape[0])
+
+    all_features_df = pd.concat([features_df_with, features_df_without])
+    scaled = MinMaxScaler(feature_range=(0, 1)).fit_transform(all_features_df)
+    all_features_df = pd.DataFrame(scaled, columns=all_features_df.columns)
+    label_df = all_features_df.pop(CLASSIFICATION_LABEL_FIELD)
+
+    return all_features_df, label_df
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    info("tensorflow version: {}".format(tf.__version__))
+
+    model_type = "classification"  # change this to run either regression or classification
+
+    if model_type == "classification":
+        features_df, label_df = get_data_for_classification_model()
+        classification_model: tf.keras.Model = create_and_train_classification_model(features_df, label_df)
+        classification_model.save("my_classification_model.h5")
+        loaded_model: tf.keras.Model = tf.keras.models.load_model("my_classification_model.h5")
+        loaded_model.summary()
+    else:  # regression
+        features_df, label_df = get_data_for_regression_model()
+        regression_model: tf.keras.Model = create_and_train_regression_model(features_df, label_df)
+        regression_model.save("my_regression_model.h5")
+        loaded_model: tf.keras.Model = tf.keras.models.load_model("my_regression_model.h5")
+        loaded_model.summary()
+
+    # validation_results = loaded_model.evaluate(features_df, label_df, batch_size=128, return_dict=True, verbose=1)
     y_pred = loaded_model.predict(features_df)
 
     info(f"Predictions shape: {y_pred.shape}")
