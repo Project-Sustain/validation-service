345e40b1f74d07ac11bf296f349cda47189fc7e0
Caleb Carlson <ccarlson355@gmail.com>
2022-03-23 12:52:26 -0600

    Add benchmark script

--- benchmarks/start_omni.sh
+++ benchmarks/start_omni.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 function print_usage {
-  echo -e "./start_benchmark.sh <name>"
-  echo -e "EXAMPLE ./start_benchmark.sh sh13rs_gisjoins_vs"
+  echo -e "./start_omni.sh <name>"
+  echo -e "EXAMPLE ./start_omni.sh sh13rs_gisjoins_vs"
 }
 
 [[ $# -eq 1 ]] || (print_usage; exit 1)
