4e2ada4550f235c039dfc724d94e30df43170caf
Caleb Carlson <ccarlson355@gmail.com>
2022-03-24 09:18:54 -0600

    Update run script

---
+++ benchmarks/make_request.py
@@ -0,0 +1,28 @@
+import requests
+import json
+from pprint import pprint
+
+
+url = "lattice-150.cs.colostate.edu:5000/validation_service/submit_validation_job"
+request_file = "/s/parsons/b/others/sustain/SustainProject/validation-service/testing/test_requests/test_request_all_gis_joins.json"
+model_file = "/s/parsons/b/others/sustain/SustainProject/validation-service/testing/test_models/tensorflow/linear_regression/hdf5/my_model.h5"
+
+with open(request_file, "r") as rfile:
+    request = json.load(rfile)
+
+payload = {"request": request}
+
+pprint(payload)
+
+# files = [
+#   ('file', ('my_model.h5',
+#             open(model_file, 'rb'),
+#             'application/octet-stream')
+#    )
+# ]
+#
+# headers = {}
+#
+# response = requests.request("POST", url, headers=headers, data=payload, files=files)
+#
+# print(response.text)
--- benchmarks/run_benchmark.sh
+++ benchmarks/run_benchmark.sh
@@ -12,12 +12,10 @@ fi
 
 BENCHMARK_NAME="$1"
 
-echo "Starting omni for $BENCHMARK_NAME benchmark..."
-./start_omni.sh "$BENCHMARK_NAME"
+#echo "Starting omni for $BENCHMARK_NAME benchmark..."
+#./start_omni.sh "$BENCHMARK_NAME"
 
-curl --location --request POST 'lattice-150.cs.colostate.edu:5000/validation_service/submit_validation_job' \
---form 'file=@"/s/parsons/b/others/sustain/SustainProject/validation-service/testing/test_models/tensorflow/linear_regression/hdf5/my_model.h5"' \
---form 'request=@"/s/parsons/b/others/sustain/SustainProject/validation-service/testing/test_requests/test_request_all_gis_joins.json"' > response.txt
+python3.8 make_request.py
 
-echo "Stopping omni for $BENCHMARK_NAME benchmark..."
-./stop_omni.sh
+#echo "Stopping omni for $BENCHMARK_NAME benchmark..."
+#./stop_omni.sh
