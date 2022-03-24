50da80bf84441a29f2741a30bd749f67e0dfe711
Sustain Account <sustain@cs.colostate.edu>
2022-03-24 12:54:22 -0600

    Fix kill worker script

--- kill_worker.sh
+++ kill_worker.sh
@@ -5,7 +5,7 @@ WORKER_PROCESSES=$(ps -aux | grep "[o]verlay --worker")
 if [[ $WORKER_PROCESSES != "" ]]; then
   echo "Killing with SIGINT: $WORKER_PROCESSES"
   # Send SIGINT signal to worker process, shutting it down gracefully
-  kill -INT "$(ps -aux | grep "[o]verlay --worker" | awk '{ print $2 }')"
+  pkill -f overlay
 else
   echo "Did not find any worker processes to kill"
 fi
