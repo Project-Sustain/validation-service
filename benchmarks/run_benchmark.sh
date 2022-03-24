84ea114cfa5ec7c137764c10e55b024821bde09b
Caleb Carlson <ccarlson355@gmail.com>
2022-03-24 08:59:41 -0600

    Update run script

--- benchmarks/run_benchmark.sh
+++ benchmarks/run_benchmark.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 function print_usage {
-  echo -e "./run_benchmark.sh <name> <json_request_file>"
-  echo -e "EXAMPLE ./run_benchmark.sh sh13rs_gisjoins_vs ../testing/test_requests/test_request_all_gis_joins.json"
+  echo -e "./run_benchmark.sh <name>"
+  echo -e "EXAMPLE ./run_benchmark.sh sh13rs_gisjoins_vs"
 }
 
 [[ $# -eq 2 ]] || print_usage && exit 1
@@ -16,8 +16,8 @@ echo "Starting omni for $BENCHMARK_NAME benchmark..."
 ./start_omni.sh "$BENCHMARK_NAME"
 
 curl --location --request POST 'lattice-150.cs.colostate.edu:5000/validation_service/submit_validation_job' \
---form 'file=@"../testing/test_models/tensorflow/linear_regression/hdf5/my_model.h5"' \
---form "request=@${REQUEST_FILE_NAME}" > response.txt
+--form 'file=@"/s/parsons/b/others/sustain/SustainProject/validation-service/testing/test_models/tensorflow/linear_regression/hdf5/my_model.h5"' \
+--form 'request=@"/s/parsons/b/others/sustain/SustainProject/validation-service/testing/test_requests/test_request_all_gis_joins.json"' > response.txt
 
 echo "Stopping omni for $BENCHMARK_NAME benchmark..."
 ./stop_omni.sh
