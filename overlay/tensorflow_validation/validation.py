d669fd328a3746948cf6f1c1dcf9396753f7c54e
Caleb Carlson <ccarlson355@gmail.com>
2022-04-12 14:01:18 -0600

    Debugging

--- overlay/tensorflow_validation/validation.py
+++ overlay/tensorflow_validation/validation.py
@@ -271,7 +271,7 @@ def validate_model(
 
     if loss_function == "MEAN_SQUARED_ERROR":
         info("MEAN_SQUARED_ERROR...")
-        loss = tf.reduce_mean(tf.square(tf.subtract(y_true, y_pred)))
+        #loss = tf.reduce_mean(tf.square(tf.subtract(y_true, y_pred)))
 
         # Empirical Variance implementation for MSE
         # N_h = gis_join_count
@@ -288,21 +288,22 @@ def validate_model(
         # Numpy's built-in variance function for MSE
         squared_residuals = np.square(y_true - y_pred)
         m = np.mean(squared_residuals, axis=0)[0]
+        loss = m
         s = (np.var(squared_residuals, axis=0, ddof=0) * squared_residuals.shape[0])[0]
-
-        info(f"m = {m}, s = {s}")
+        info(f"m = {m}, s = {s}, loss = {loss}")
 
     elif loss_function == "ROOT_MEAN_SQUARED_ERROR":
         info("ROOT_MEAN_SQUARED_ERROR...")
-        loss = tf.sqrt(tf.reduce_mean(tf.square(tf.subtract(y_true, y_pred))))
+        #loss = tf.sqrt(tf.reduce_mean(tf.square(tf.subtract(y_true, y_pred))))
         squared_residuals = np.square(y_true - y_pred)
         m = np.mean(squared_residuals, axis=0)[0]
+        loss = sqrt(m)
         s = (np.var(squared_residuals, axis=0, ddof=0) * squared_residuals.shape[0])[0]
 
     elif loss_function == "MEAN_ABSOLUTE_ERROR":
         info("MEAN_ABSOLUTE_ERROR...")
-        loss = np.mean(np.abs(y_true - y_pred), axis=0)[0]
-        absolute_residuals = np.absolute(y_pred - y_true)
+        absolute_residuals = np.abs(y_true - y_pred)
+        loss = np.mean(absolute_residuals, axis=0)[0]
         m = np.mean(absolute_residuals, axis=0)[0]
         s = (np.var(absolute_residuals, axis=0, ddof=0) * absolute_residuals.shape[0])[0]
 
