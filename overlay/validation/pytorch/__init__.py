c0cc562e52c02c47c072065700165f9c22d0b409
Caleb Carlson <ccarlson355@gmail.com>
2022-05-02 10:06:14 -0600

    Convert numpy float32 to python float

--- overlay/validation/pytorch/__init__.py
+++ overlay/validation/pytorch/__init__.py
@@ -146,24 +146,24 @@ def validate_model(
     if loss_function == "MEAN_ABSOLUTE_ERROR":
         info("MEAN_ABSOLUTE_ERROR...")
         absolute_residuals = np.abs(y_true_numpy - y_predicted_numpy)
-        loss = np.mean(absolute_residuals, axis=0)
-        m = np.mean(absolute_residuals, axis=0)[0]
-        s = (np.var(absolute_residuals, axis=0, ddof=0) * absolute_residuals.shape[0])
+        m = np.mean(absolute_residuals, axis=0).item()
+        loss = m
+        s = (np.var(absolute_residuals, axis=0, ddof=0) * absolute_residuals.shape[0]).item()
 
     elif loss_function == "MEAN_SQUARED_ERROR":
         info("MEAN_SQUARED_ERROR...")
         squared_residuals = np.square(y_true_numpy - y_predicted_numpy)
         info(f"squared_residuals: {squared_residuals}")
-        m = np.mean(squared_residuals, axis=0)
+        m = np.mean(squared_residuals, axis=0).item()
         loss = m
-        s = (np.var(squared_residuals, axis=0, ddof=0) * squared_residuals.shape[0])
+        s = (np.var(squared_residuals, axis=0, ddof=0) * squared_residuals.shape[0]).item()
 
     elif loss_function == "ROOT_MEAN_SQUARED_ERROR":
         info("ROOT_MEAN_SQUARED_ERROR...")
         squared_residuals = np.square(y_true_numpy - y_predicted_numpy)
-        m = np.mean(squared_residuals, axis=0)
+        m = np.mean(squared_residuals, axis=0).item()
         loss = sqrt(m)
-        s = (np.var(squared_residuals, axis=0, ddof=0) * squared_residuals.shape[0])
+        s = (np.var(squared_residuals, axis=0, ddof=0) * squared_residuals.shape[0]).item()
 
     elif loss_function == "NEGATIVE_LOG_LIKELIHOOD_LOSS":
         profiler.stop()
