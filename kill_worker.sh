81309c0f244259fd46f2bb38bfc6aba2348559d1
Sustain Account <sustain@cs.colostate.edu>
2022-05-09 11:26:05 -0600

    Update kill_worker.sh script

--- kill_worker.sh
+++ kill_worker.sh
@@ -6,6 +6,7 @@ if [[ $WORKER_PROCESSES != "" ]]; then
   echo "Killing with SIGINT: $WORKER_PROCESSES"
   # Send SIGINT signal to worker process, shutting it down gracefully
   pkill -f overlay
+  kill $(ps -aux | grep -P "[l]oky" | awk '{ print $2 }')
 else
   echo "Did not find any worker processes to kill"
 fi
