29d7b8f2c7ba6d9e499e2e9d1da00731675a3233
Caleb Carlson <ccarlson355@gmail.com>
2022-05-11 12:00:55 -0600

    Use python requests

--- testing/streaming/make_request.py
+++ testing/streaming/make_request.py
@@ -4,6 +4,7 @@ from codecs import encode
 import json
 import sys
 import os
+import requests
 
 experiment_request = f"request.json"
 model_file = f"model.h5"
@@ -18,33 +19,47 @@ request_file = f"request.json"
 with open(request_file, "r") as rfile:
     request = json.load(rfile)
 
-conn = http.client.HTTPConnection("lattice-150.cs.colostate.edu", 5000)
-dataList = []
-boundary = 'wL36Yn8afVp8Ag7AmP8qZ0SA4n1v9T'
-dataList.append(encode('--' + boundary))
-dataList.append(encode(f"Content-Disposition: form-data; name=file; filename={model_file}"))
-
-fileType = mimetypes.guess_type(model_file)[0] or 'application/octet-stream'
-dataList.append(encode('Content-Type: {}'.format(fileType)))
-dataList.append(encode(''))
-
-with open(model_file, 'rb') as f:
-  dataList.append(f.read())
-dataList.append(encode('--' + boundary))
-dataList.append(encode('Content-Disposition: form-data; name=request;'))
-
-dataList.append(encode('Content-Type: {}'.format('text/plain')))
-dataList.append(encode(''))
-dataList.append(encode(json.dumps(request)))
-
-dataList.append(encode('--'+boundary+'--'))
-dataList.append(encode(''))
-body = b'\r\n'.join(dataList)
-payload = body
 headers = {
-   'Content-type': 'multipart/form-data; boundary={}'.format(boundary)
+    'Content-Type': 'multipart/form-data; boundary=--------------------------531818130631649698349478'
 }
-conn.request("POST", "/validation_service/submit_validation_job", payload, headers)
-res = conn.getresponse()
-data = res.read()
-print(data.decode("utf-8"))
+
+files = [
+  ('file', ('model.h5', open('model.h5', 'rb'), 'application/octet-stream'))
+]
+payload = {
+    "request": json.dumps(request)
+}
+
+response = requests.request("POST", url, headers=headers, data=payload, files=files)
+print(response.text.encode('utf8'))
+#
+# conn = http.client.HTTPConnection("lattice-150.cs.colostate.edu", 5000)
+# dataList = []
+# boundary = 'wL36Yn8afVp8Ag7AmP8qZ0SA4n1v9T'
+# dataList.append(encode('--' + boundary))
+# dataList.append(encode(f"Content-Disposition: form-data; name=file; filename={model_file}"))
+#
+# fileType = mimetypes.guess_type(model_file)[0] or 'application/octet-stream'
+# dataList.append(encode('Content-Type: {}'.format(fileType)))
+# dataList.append(encode(''))
+#
+# with open(model_file, 'rb') as f:
+#   dataList.append(f.read())
+# dataList.append(encode('--' + boundary))
+# dataList.append(encode('Content-Disposition: form-data; name=request;'))
+#
+# dataList.append(encode('Content-Type: {}'.format('text/plain')))
+# dataList.append(encode(''))
+# dataList.append(encode(json.dumps(request)))
+#
+# dataList.append(encode('--'+boundary+'--'))
+# dataList.append(encode(''))
+# body = b'\r\n'.join(dataList)
+# payload = body
+# headers = {
+#    'Content-type': 'multipart/form-data; boundary={}'.format(boundary)
+# }
+# conn.request("POST", "/validation_service/submit_validation_job", payload, headers)
+# res = conn.getresponse()
+# data = res.read()
+# print(data.decode("utf-8"))
