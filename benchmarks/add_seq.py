ae2ebcbf802a7e2eaf6f2de917065ecd5dd661e6
Caleb Carlson <ccarlson355@gmail.com>
2022-05-19 12:07:48 -0600

    Add sequence script

--- benchmarks/add_seq.py
+++ benchmarks/add_seq.py
@@ -9,13 +9,13 @@ def main():
     client = MongoClient("mongodb://localhost:27018")
     db = client["sustaindb"]
     col = db["noaa_nam"]
-    total = col.count_documents(filter={})
-    all_docs_cursor = col.find(filter={})
+    total = 83221482
+    all_docs_cursor = col.find()
     seq = 0
     bar = ProgressBar(maxval=total, widgets=widgets).start()
     for doc in all_docs_cursor:
         doc_id = doc["_id"]
-        col.find_one_and_update(filter={"_id": doc_id}, update={"$set": {"SEQUENCE": seq}})
+        col.update_one({"_id": doc_id}, {"$set": {"SEQUENCE": seq}})
         seq += 1
 
         if seq % 100 == 0:
