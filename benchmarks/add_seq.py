25319b88abaf9e703c83b61eaed8932a3b3c4a2d
Caleb Carlson <ccarlson355@gmail.com>
2022-05-19 12:01:57 -0600

    Add sequence script

---
+++ benchmarks/add_seq.py
@@ -0,0 +1,28 @@
+from pymongo import MongoClient
+from progressbar import ProgressBar, Bar, Percentage, SimpleProgress, Timer
+
+# Progress Bar widgets
+widgets = [SimpleProgress(), Percentage(), Bar(), Timer()]
+
+
+def main():
+    client = MongoClient("mongodb://localhost:27018")
+    db = client["sustaindb"]
+    col = db["noaa_nam"]
+    total = col.count_documents()
+    all_docs_cursor = col.find()
+    seq = 0
+    bar = ProgressBar(maxval=total, widgets=widgets).start()
+    for doc in all_docs_cursor:
+        doc_id = doc["_id"]
+        col.find_one_and_update({"_id": doc_id}, {"$set": {"SEQUENCE": seq}})
+        seq += 1
+
+        if seq % 100 == 0:
+            bar.update(seq)
+
+    bar.finish()
+
+
+if __name__ == "__main__":
+    main()
