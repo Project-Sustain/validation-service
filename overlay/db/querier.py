64063c7361a64bf13fe12dbf657fdee1d9ef4c84
kmbear3 <kmbarram@gmail.com>
2022-06-15 17:46:43 -0600

    hardcode values

--- overlay/db/querier.py
+++ overlay/db/querier.py
@@ -23,13 +23,16 @@ class Querier:
         # username = urllib.parse.quote_plus(os.environ.get('READ_MONGO_USER'))
         # password = urllib.parse.quote_plus(os.environ.get('READ_MONGO_PASS'))
 
-        username = "root"
-        password = "rootPass"
-
-
-        self.mongo_uri = f"mongodb://{username}:{password}@{mongo_host}:{mongo_port}"
+        # username = "root"
+        # password = "rootPass"
+        #
+        #
+        # self.mongo_uri = f"mongodb://{username}:{password}@{mongo_host}:{mongo_port}"
 
         # end authorization
+
+        self.mongo_uri = f"mongodb://{mongo_host}:{mongo_port}"  # Old URI pre authorization
+
         self.replica_set_status = locality.get_replica_set_status()
         self.db_name = db_name
         self.read_preference = read_preference
