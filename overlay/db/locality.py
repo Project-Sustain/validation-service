8d3331be557c3266297782e22f4405cb7767d063
Sustain Account <sustain@cs.colostate.edu>
2022-06-19 11:20:38 -0600

    trying again

--- overlay/db/locality.py
+++ overlay/db/locality.py
@@ -27,6 +27,8 @@ def discover_gis_joins() -> dict:
     gis_join_counts: dict = {}  # { gis_join -> count }
     info("Inside locality.py, just above the call to mongod")
     # client: MongoClient = MongoClient("mongodb://localhost:27017")
+    username = "root"
+    password = "rootPass"
     client: MongoClient = MongoClient(f"mongodb://{username}:{password}@localhost:27017")
     info("below error")
     db = client["sustaindb"]
