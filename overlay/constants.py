c838d44ca070cefdf52ce0318c8c5135793aaa0a
Sustain Account <sustain@cs.colostate.edu>
2022-06-19 11:48:08 -0600

    bug

--- overlay/constants.py
+++ overlay/constants.py
@@ -9,8 +9,8 @@ DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = int(os.environ.get("DB_PORT"))
 DB_NAME = os.environ.get("DB_NAME")
 MODELS_DIR = os.environ.get("MODELS_DIR")
-username = urllib.parse.quote_plus(os.environ.get('ROOT_MONGO_USER'))
-password = urllib.parse.quote_plus(os.environ.get('ROOT_MONGO_PASS'))
+username = urllib.parse.quote_plus(str(os.environ.get('ROOT_MONGO_USER')))
+password = urllib.parse.quote_plus(str(os.environ.get('ROOT_MONGO_PASS')))
 
 
 if __name__ == "__main__":
