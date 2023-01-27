4d1cf0322a8470951c1f3f4d0be97c40a8027914
Menuka Warushavithana <menuka.warushavithana@gmail.com>
2023-01-26 23:46:59 -0800

    classification(sklearn): build_full_response - 1

---
+++ classification-test/sklearn/build_full_respose.py
@@ -0,0 +1,23 @@
+# Build a complete classification response for frontend testing
+import pymongo
+import pandas as pd
+import os
+import json
+import pickle
+from sklearn.preprocessing import MinMaxScaler
+from sklearn import metrics
+
+MONGO_USER = "root"
+MONGO_PASS = "rootPass"
+MONGO_HOST = "lattice-101"
+MONGO_PORT = 27018
+MONGO_URL = f"mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_HOST}:{MONGO_PORT}"
+DB_NAME = "sustaindb"
+
+db_connection = pymongo.MongoClient(MONGO_URL)
+db = db_connection[DB_NAME]
+noaa = db['noaa_nam']
+
+single_raw_data = noaa.find({'GISJOIN': 'G0600470'})
+df = pd.DataFrame(list(single_raw_data))
+print(df)
