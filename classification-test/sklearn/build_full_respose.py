87dbc58b2658391165734510635ff02fcd0e1429
Menuka Warushavithana <menuka.warushavithana@gmail.com>
2023-01-27 00:03:48 -0800

    classification(sklearn): build_full_response - 3

--- classification-test/sklearn/build_full_respose.py
+++ classification-test/sklearn/build_full_respose.py
@@ -14,6 +14,18 @@ MONGO_PORT = 27018
 MONGO_URL = f"mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_HOST}:{MONGO_PORT}"
 DB_NAME = "sustaindb"
 
+FEATURES_FIELDS = [
+    "PRESSURE_REDUCED_TO_MSL_PASCAL",
+    "VISIBILITY_AT_SURFACE_METERS",
+    "VISIBILITY_AT_CLOUD_TOP_METERS",
+    "WIND_GUST_SPEED_AT_SURFACE_METERS_PER_SEC",
+    "PRESSURE_AT_SURFACE_PASCAL",
+]
+
+LABEL_FIELD = [
+    "CATEGORICAL_SNOW_SURFACE_BINARY"
+]
+
 db_connection = pymongo.MongoClient(MONGO_URL)
 db = db_connection[DB_NAME]
 noaa = db['noaa_nam']
@@ -25,5 +37,21 @@ print(df)
 
 print('---------------------')
 gis_joins = noaa.distinct('GISJOIN')
-print(len(gis_joins))
+print(f'No. of GISJOINs: {len(gis_joins)}')
+print('---------------------\n')
 
+# Load trained model
+model = pickle.load(open('model.pkl', 'rb'))
+features_df = pd.DataFrame(list(single_raw_data))
+scaled = MinMaxScaler(feature_range=(0, 1)).fit_transform(features_df)
+features_df = pd.DataFrame(scaled, columns=features_df.columns)
+
+label_df = features_df.pop(LABEL_FIELD)
+features_df = features_df[FEATURES_FIELDS]
+
+inputs_numpy = features_df.to_numpy()
+y_true = label_df.to_numpy()
+y_pred_class = model.predict(inputs_numpy)
+
+accuracy = metrics.accuracy_score(y_true, y_pred_class)
+print(f'Accuracy: {accuracy}')
