93ff5a39fab310e8b25c1a3e3e1c77019473771f
Menuka Warushavithana <menuka.warushavithana@gmail.com>
2023-02-07 01:21:20 -0800

    sklearn: update classification streaming
    use sample response

--- overlay/validation/scikitlearn/__init__.py
+++ overlay/validation/scikitlearn/__init__.py
@@ -152,18 +152,18 @@ def validate_classification_model(
     # for t in thresholds:
     #     y_pred_prob = (model.predict_proba(inputs_numpy)[:, 1] >= t).astype(int)
 
-        # Calculate Precision
-        # precision = metrics.precision_score(y_true, y_pred_class, zero_division=0)
-        # logger.success(f"Precision (t = {t}): {precision}")
+    # Calculate Precision
+    # precision = metrics.precision_score(y_true, y_pred_class, zero_division=0)
+    # logger.success(f"Precision (t = {t}): {precision}")
 
-        # Calculate Recall
-        # recall = metrics.recall_score(y_true, y_pred_class, zero_division=0)
-        # logger.success(f"Recall (t = {t}): {recall}")
+    # Calculate Recall
+    # recall = metrics.recall_score(y_true, y_pred_class, zero_division=0)
+    # logger.success(f"Recall (t = {t}): {recall}")
 
-        # ROC Curves and Area Under the Curve (AUC)
-        # fpr, tpr, thresholds = metrics.roc_curve(y_true, y_pred_prob)
-        # roc_auc_score = metrics.roc_auc_score(y_true, y_pred_prob)
-        # logger.success(f"roc_auc_score (t = {t}): {roc_auc_score}")
+    # ROC Curves and Area Under the Curve (AUC)
+    # fpr, tpr, thresholds = metrics.roc_curve(y_true, y_pred_prob)
+    # roc_auc_score = metrics.roc_auc_score(y_true, y_pred_prob)
+    # logger.success(f"roc_auc_score (t = {t}): {roc_auc_score}")
 
     # def evaluate_threshold(threshold):
     #     sensitivity = tpr[thresholds > threshold][-1]
@@ -176,58 +176,7 @@ def validate_classification_model(
     # raise NotImplementedError("validate_classification_model() is not implemented for class ScikitLearnValidator.")
     logger.debug(f"Returning GISJOIN: {gis_join}")
     # Returns the gis_join, ok_status, response, error_msg
-    result = '''
-    {
-        "auc_of_roc": 0.5,
-        "roc_graph": {
-            "x_coordinates": [
-                0.0,
-                1.0
-            ],
-            "y_coordinates": [
-                0.0,
-                1.0
-            ]
-        },
-        "0.1": {
-            "precision": 0.3476,
-            "recall": 1.0
-        },
-        "0.2": {
-            "precision": 0.4296,
-            "recall": 0.9385
-        },
-        "0.3": {
-            "precision": 0.4752,
-            "recall": 0.7385
-        },
-        "0.4": {
-            "precision": 0.4286,
-            "recall": 0.4154
-        },
-        "0.5": {
-            "precision": 0.4865,
-            "recall": 0.2769
-        },
-        "0.6": {
-            "precision": 0.5,
-            "recall": 0.1231
-        },
-        "0.7": {
-            "precision": 0.5,
-            "recall": 0.0769
-        },
-        "0.8": {
-            "precision": 1.0,
-            "recall": 0.0462
-        },
-        "0.9": {
-            "precision": 0.0,
-            "recall": 0.0
-        }
-    }
-    '''
-    result = json.dumps(result, separators=(',', ':'))
+    result = '{"auc_of_roc":0.5,"roc_graph":{"x_coordinates":[0,1],"y_coordinates":[0,1]},"0.1":{"precision":0.3476,"recall":1},"0.2":{"precision":0.4296,"recall":0.9385},"0.3":{"precision":0.4752,"recall":0.7385},"0.4":{"precision":0.4286,"recall":0.4154},"0.5":{"precision":0.4865,"recall":0.2769},"0.6":{"precision":0.5,"recall":0.1231},"0.7":{"precision":0.5,"recall":0.0769},"0.8":{"precision":1,"recall":0.0462},"0.9":{"precision":0,"recall":0}}'
     return gis_join, True, result, ""
 
 
