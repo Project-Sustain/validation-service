e51fd27afaece04f558a70eb0cb4f98ea6f6705d
Menuka Warushavithana <menuka.warushavithana@gmail.com>
2023-02-08 22:11:01 -0700

    refactor: update flask logs

--- overlay/flask_server.py
+++ overlay/flask_server.py
@@ -242,7 +242,7 @@ def validation():
                     logger.info("==========================================================")
 
                     for validation_grpc_response in stub.SubmitValidationJob(validation_grpc_request):
-                        logger.info(f"inside flask server!! {validation_grpc_response}")
+                        logger.trace(f"{validation_grpc_response}")
                         dict_response = MessageToDict(validation_grpc_response, preserving_proto_field_name=True)
                         yield json.dumps(dict_response, indent=None) + '\n'
                         # yield build_json_response(validation_grpc_response)
